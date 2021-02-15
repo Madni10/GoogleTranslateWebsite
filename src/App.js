5d7e0e243cb18e4a17f8669ca77240b909e19fb4
Ahmed Madni <ahmed.madni@hyperdatacomputing.com>
2021-02-15 18:17:29 +0500

    First Commit

---
+++ src/App.js
@@ -0,0 +1,32 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  
+
+  return (
+  
+<div >  
+    <p>Hello everyone!</p>  
+    <p>Welcome to GeeksforGeeks</p>  
+      
+    <p> 
+        Translate this page in 
+        your preferred language: 
+    </p>  
+      
+    <div id="google_translate_element">
+    <p> 
+        You can translate the content of this 
+        page by selecting a language in the 
+        select box. 
+    </p>  
+      </div>  
+   
+</div>  
+  
+
+  );
+}
+
+export default App;
